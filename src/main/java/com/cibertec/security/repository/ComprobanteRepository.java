ce140c1c666b7bc73894d82f94f4e67beb17e363
daniel <kaledaniebt@gmail.com>
2025-08-19 20:50:10 -0500

    Fernanda termino

--- src/main/java/com/cibertec/security/repository/ComprobanteRepository.java
+++ src/main/java/com/cibertec/security/repository/ComprobanteRepository.java
@@ -1,4 +1,5 @@
 package com.cibertec.security.repository;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -10,28 +11,20 @@ import com.cibertec.security.entity.Comprobante;
 
 public interface ComprobanteRepository extends JpaRepository<Comprobante, Long> {
 
-
-@Query("""
-    SELECT c FROM Comprobante c
-    WHERE (:idPedido = 0 OR c.pedido.id = :idPedido)
-      AND (:idCliente = 0 OR c.cliente.id = :idCliente)
-      AND (:idEmpleado = 0 OR c.empleado.id = :idEmpleado)
-      AND (:idCaja = 0 OR c.caja.id = :idCaja)
-      AND (:idEstado = 0 OR c.estado.id = :idEstado)
-      AND (
-            (:fechaInicio IS NULL OR :fechaFin IS NULL)
-            OR (c.fechaEmision BETWEEN :fechaInicio AND :fechaFin)
-          )
-""")
-List<Comprobante> buscarComprobantesFiltro(
-    @Param("idPedido") Long idPedido,
-    @Param("idCliente") Long idCliente,
-    @Param("idEmpleado") Long idEmpleado,
-    @Param("idCaja") Long idCaja,
-    @Param("idEstado") Long idEstado,
-    @Param("fechaInicio") LocalDateTime fechaInicio,
-    @Param("fechaFin") LocalDateTime fechaFin
-);
-
+	@Query("""
+			    SELECT c FROM Comprobante c
+			    WHERE (:idPedido = 0 OR c.pedido.id = :idPedido)
+			      AND (:idCliente = 0 OR c.cliente.id = :idCliente)
+			      AND (:idEmpleado = 0 OR c.empleado.id = :idEmpleado)
+			      AND (:idCaja = 0 OR c.caja.id = :idCaja)
+			      AND (:idEstado = 0 OR c.estado.id = :idEstado)
+			      AND (
+			            (:fechaInicio IS NULL OR :fechaFin IS NULL)
+			            OR (c.fechaEmision BETWEEN :fechaInicio AND :fechaFin)
+			          )
+			""")
+	List<Comprobante> buscarComprobantesFiltro(@Param("idPedido") Long idPedido, @Param("idCliente") Long idCliente,
+			@Param("idEmpleado") Long idEmpleado, @Param("idCaja") Long idCaja, @Param("idEstado") Long idEstado,
+			@Param("fechaInicio") LocalDateTime fechaInicio, @Param("fechaFin") LocalDateTime fechaFin);
 
 }
